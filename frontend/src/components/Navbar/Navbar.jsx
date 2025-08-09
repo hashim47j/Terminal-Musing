a04463d3c8ce7a25408726ad5f881a72a0070e1a
Hash <you@example.com>
2025-08-09 15:18:56 -0400

    hehehe

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -273,7 +273,7 @@ const Navbar = () => {
         style={{ 
           width: !isMobileView && leftNavbarWidth ? `${leftNavbarWidth}px` : "auto",
           ...(isBlogPostPage && { 
-            top: isMobileView ? '100px' : '110px'
+            top: isMobileView ? '95px' : '110px'
           })
         }}
       >

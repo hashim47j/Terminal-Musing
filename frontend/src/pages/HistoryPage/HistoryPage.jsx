02f3ada1e2b3a799518d4395e5bf895626444681
Hash <you@example.com>
2025-08-27 13:21:53 -0400

    colored shadows

--- frontend/src/pages/HistoryPage/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage/HistoryPage.jsx
@@ -299,9 +299,7 @@ const HistoryPage = () => {
       <DynamicBackgroundShadow />
 
       {/* Fixed Posts Heading */}
-      <div className={styles.postsHeadingSection}>
-        <h2 className={styles.postsHeading}>History Posts</h2>
-      </div>
+
 
       {/* Scrollable Posts Section */}
       <section className={styles.postsSection}>

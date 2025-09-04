ee792b38a404bac0735af4a6ab057f7c05493654
Hash <you@example.com>
2025-09-04 03:41:33 -0400

    Update package-lock.json before merge

--- frontend/src/components/pageanim/PageMapper.jsx
+++ frontend/src/components/pageanim/PageMapper.jsx
@@ -1,20 +1,18 @@
+// frontend/src/components/pageanim/PageMapper.jsx
 import HomePage from '../../pages/HomePage';
-import PhilosophyPage from '../../pages/PhilosophyPage';
-import HistoryPage from '../../pages/HistoryPage/HistoryPage';
-import WritingsPage from '../../pages/WritingsPage/WritingsPage';
-import LsconcernPage from '../../pages/LsconcernPage/LsconcernPage';
-import TechPage from '../../pages/TechPage/TechPage';
+import UniformPage from '../../pages/UniformPages/UniformPage';
 import DailythoughtsReader from '../../pages/DailythoughtsPage/DailythoughtsReader';
 import AdminLogin from '../../pages/AdminPage/AdminLogin';
 
 const getPageComponent = (path) => {
   switch (path) {
     case '/': return <HomePage />;
-    case '/philosophy': return <PhilosophyPage />;
-    case '/history': return <HistoryPage />;
-    case '/writings': return <WritingsPage />;
-    case '/legal-social': return <LsconcernPage />;
-    case '/tech': return <TechPage />;
+    case '/philosophy':
+    case '/history':
+    case '/writings':
+    case '/legal-social':
+    case '/tech':
+      return <UniformPage />;
     case '/daily-thoughts': return <DailythoughtsReader />;
     case '/admin/login': return <AdminLogin />;
     default: return null;
